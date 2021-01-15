8b4e15513424f1dec347cac3611c18bd2465b5d4
Jeremy Miller <gkstaff@gmail.com>
2021-01-15 11:36:11 -0800

    This commit adds scripting to assign the radios and input fields to values for logic scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
+  
+  
+  $("#submitButton").click(function(){
+      const compPreferences = $("input:radio[name=compPref]:checked").val();
+      const phonePreferences = $("input:radio[name=phonePref]:checked").val();
+      const name = $('#inputName').val();
+      const age = $('#inputAge').val();
+      const preferedNoun = $("#inputNoun").val();
     
-    $("#textmiddle").click(function(){
-        
-        
+      
+      alert(name);
+      alert(age);
+      alert(preferedNoun);
+      alert(compPreferences);
+      alert(phonePreferences);
+      event.preventDefault();
+
     });
 });
 
-function moveBox() {
+
+/*function moveBox() {
     $(".tree").animate({
       "top": 20,
       "left": 20
     });
-  }
+  }*/
