917d793ed6b24cc5df92071327644335016e82bd
Jeremy Miller <gkstaff@gmail.com>
2021-01-15 09:51:25 -0800

    This commit adds in images I made in photoshop and the tables with radio and input settings. No logic currently only UI.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $("#textmiddle").click(function(){
+        
+        
+    });
+});
+
+function moveBox() {
+    $(".tree").animate({
+      "top": 20,
+      "left": 20
+    });
+  }
