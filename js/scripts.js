29bc1ae42000a1268b3c7a36e1144d92888ef4bc
Jeremy Miller <gkstaff@gmail.com>
2021-01-15 13:30:36 -0800

    This commit is adding more logic that was not working before! Now radios are being assigned their respective val on check.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
-$(document).ready(function(){
-  
-  
-  $("#submitButton").click(function(){
-      const compPreferences = $("input:radio[name=compPref]:checked").val();
-      const phonePreferences = $("input:radio[name=phonePref]:checked").val();
-      const name = $('#inputName').val();
-      const age = $('#inputAge').val();
-      const preferedNoun = $("#inputNoun").val();
-    
-      
-      alert(name);
-      alert(age);
-      alert(preferedNoun);
-      alert(compPreferences);
-      alert(phonePreferences);
-      event.preventDefault();
+function macIphone(compPreferences, phonePreferences) {
+  return compPreferences + phonePreferences;
+}
+function macAndroid(compPreferences, phonePreferences) {
+  return compPreferences + phonePreferences;
+}
+function pcIphone(compPreferences, phonePreferences) {
+  return compPreferences + phonePreferences;
+}
+function pcAndroid(compPreferences, phonePreferences) {
+  return compPreferences + phonePreferences;
+}
 
-    });
+
+//Globals //
+
+const name = $('#inputName').val();
+const age = $('#inputAge').val();
+const preferedNoun = $("#inputNoun").val();
+const compPreferences = $("input:radio[name=MacOrPC]:checked").val();
+const phonePreferences = $("input:radio[name=iphoneOrAndroid]:checked").val();
+
+
+
+$(document).ready(function () {
+  $("#submitButton").click(function () {
+    const name = $('#inputName').val();
+    const age = $('#inputAge').val();
+    const preferedNoun = $("#inputNoun").val();
+    const compPreferences = $("input:radio[name=compPref]:checked").val();
+    const phonePreferences = $("input:radio[name=phonePref]:checked").val();
+    alert(compPreferences);
+    alert(phonePreferences);
+
+
+    
+    event.preventDefault();
+  });
 });
 
 
