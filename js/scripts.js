60e90de04f2c4181b9cced624bb99fbc231d8d0c
Jeremy Miller <gkstaff@gmail.com>
2021-01-15 14:55:42 -0800

    This commit is attempting to take the input values of radios to create a sum to run a function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
-function macIphone(compPreferences, phonePreferences) {
+function macIphone(compPreferences, phonePreferences){
+  
   return compPreferences + phonePreferences;
 }
-function macAndroid(compPreferences, phonePreferences) {
-  return compPreferences + phonePreferences;
-}
-function pcIphone(compPreferences, phonePreferences) {
-  return compPreferences + phonePreferences;
-}
-function pcAndroid(compPreferences, phonePreferences) {
+function macAndroid(compPreferences, phonePreferences){
   return compPreferences + phonePreferences;
 }
 
@@ -17,31 +12,21 @@ function pcAndroid(compPreferences, phonePreferences) {
 const name = $('#inputName').val();
 const age = $('#inputAge').val();
 const preferedNoun = $("#inputNoun").val();
-const compPreferences = $("input:radio[name=MacOrPC]:checked").val();
-const phonePreferences = $("input:radio[name=iphoneOrAndroid]:checked").val();
-
+const compPreferences = parseInt($("input:radio[name=mac]:checked").val());
+const phonePreferences = parseInt($("input:radio[name=iphoneOrAndroid]:checked").val());
+let radioValCombined = (compPreferences + phonePreferences);
 
 
 $(document).ready(function () {
   $("#submitButton").click(function () {
-    const name = $('#inputName').val();
-    const age = $('#inputAge').val();
-    const preferedNoun = $("#inputNoun").val();
-    const compPreferences = $("input:radio[name=compPref]:checked").val();
-    const phonePreferences = $("input:radio[name=phonePref]:checked").val();
-    alert(compPreferences);
-    alert(phonePreferences);
+  const name = $('#inputName').val();
+  const age = $('#inputAge').val();
+  const preferedNoun = $("#inputNoun").val();
+  const compPreferences = parseInt($("input:radio[name=mac]:checked").val());
+  const phonePreferences = parseInt($("input:radio[name=iphoneOrAndroid]:checked").val());
 
 
     
     event.preventDefault();
   });
 });
-
-
-/*function moveBox() {
-    $(".tree").animate({
-      "top": 20,
-      "left": 20
-    });
-  }*/
