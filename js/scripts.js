254c8e99cbc022a86a24d4a92b76dcb984f80998
Jeremy Miller <gkstaff@gmail.com>
2021-01-15 15:19:46 -0800

    This commit adds in working radio buttons. They now take the String value and sticks it with the values from second set of radios to create a unique string identifier for radio button combonation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-function macIphone(compPreferences, phonePreferences){
-  
-  return compPreferences + phonePreferences;
-}
-function macAndroid(compPreferences, phonePreferences){
+function buttonChoices(compPreferences, phonePreferences){
   return compPreferences + phonePreferences;
 }
 
@@ -22,10 +18,22 @@ $(document).ready(function () {
   const name = $('#inputName').val();
   const age = $('#inputAge').val();
   const preferedNoun = $("#inputNoun").val();
-  const compPreferences = parseInt($("input:radio[name=mac]:checked").val());
-  const phonePreferences = parseInt($("input:radio[name=iphoneOrAndroid]:checked").val());
-
-
+  const compPreferences = parseInt($("input:radio[name=compPref]:checked").val());
+  const phonePreferences = $("input:radio[name=phonePref]:checked").val();
+  const sumOfChoices = (compPreferences + phonePreferences);
+  if (sumOfChoices ==="13"){
+    alert("Mac/iPhone")
+  }
+  if (sumOfChoices==="14"){
+    alert("Mac/Android ");
+  }
+  if (sumOfChoices==="23"){
+    alert("PC/iPhone");
+  }
+  if (sumOfChoices==="24"){
+    alert("PC/Android");
+  }
+  
     
     event.preventDefault();
   });
