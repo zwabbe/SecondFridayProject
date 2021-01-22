a98b5801c3695fdfee13b5c39f8559b6fbb0d74c
Jeremy Miller <gkstaff@gmail.com>
2021-01-22 08:22:04 -0800

    Adds in fixes for images not displaying correctly and cleans up a lot of codegit add .

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,39 @@
-//Globals //
-
-const name = $('#inputName').val();
-const age = $('#inputAge').val();
-const preferedProNoun = $("#inputProNoun").val();
-const compPreferences = parseInt($("input:radio[name=compPref]:checked").val());
-const phonePreferences = $("input:radio[name=phonePref]:checked").val();
-const someNameInput = $("input#inputName").val();
-const someAgeInput = $("input#inputAge").val();
-const someProNounInput = $("input#inputProNoun").val();
-
-
-
-
-function macIphone(compPreferences, phonePreferences) {
-  const someNameInput = $("input#inputName").val();
-  const someAgeInput = $("input#inputAge").val();
-  const someProNounInput = $("input#inputProNoun").val();
-  $(".PcDisplayInput").hide();
-  $(".MacDisplayInput").fadeIn(2000);
-  $(".replacedName").text(someNameInput);
-  $(".replacedAge").text(someAgeInput);
-  $(".replacedProNoun").text(someProNounInput);
-  return macIphone;
-}
-function macAndroid(compPreferences, phonePreferences) {
-  return alert("Not native to each other so some work arounds will have to be made.");
-}
-function pcIphone(compPreferences, phonePreferences) {
-  return alert("Not native to each other so some work arounds will have to be made.");
-}
-function pcAndroid(compPreferences, phonePreferences) {
-  const someNameInput = $("input#inputName").val();
-  const someAgeInput = $("input#inputAge").val();
-  const someProNounInput = $("input#inputProNoun").val();
-  $(".MacDisplayInput").hide();
-  $(".PcDisplayInput").fadeIn(2000);
-  $(".replacedName").text(someNameInput);
-  $(".replacedAge").text(someAgeInput);
-  $(".replacedProNoun").text(someProNounInput);
-    return pcAndroid;
-}
-
 $(document).ready(function () {
-    $(".MacDisplayInput").hide();
-    $(".PcDisplayInput").hide();
-    $("#submitButton").click(function () {
-    const compPreferences = parseInt($("input:radio[name=compPref]:checked").val());
-    const phonePreferences = $("input:radio[name=phonePref]:checked").val();
-    const sumOfChoices = (compPreferences + phonePreferences);
-    const age = $('#inputAge').val();
-    let result;
-    if (sumOfChoices === "13") {
-      result = macIphone(compPreferences, phonePreferences);
+
+  $(".resultShow1").hide();
+  $(".resultShow2").hide();
+  $(".resultShow3").hide();
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+    const q1 = parseInt($("input:radio[name=q1]:checked").val());
+    const q2 = parseInt($("input:radio[name=q2]:checked").val());
+    const q3 = parseInt($("input:radio[name=q3]:checked").val());
+    const q4 = parseInt($("input:radio[name=q4]:checked").val());
+    const q5 = parseInt($("input:radio[name=q5]:checked").val());
+    const someNameInput = $("input#inputName").val();
+    let sumOfChoices = q1 + q2 + q3 + q4 + q5;
+  
+    if (someNameInput === "Name") {
+      alert("Please enter your name!")
     }
-    else if (sumOfChoices === "14") {
-      result = macAndroid(compPreferences, phonePreferences);
+    else if (sumOfChoices === 2 && 4) {
+      $(".resultShow2").hide();
+      $(".resultShow3").hide();
+      $(".resultShow1").fadeIn(2000);
+      $(".replacedName").text(someNameInput);
     }
-    if (sumOfChoices === "23") {
-      result = pcIphone(compPreferences, phonePreferences);
+    else if (sumOfChoices === 1 && 3) {
+      $(".resultShow1").hide();
+      $(".resultShow3").hide();
+      $(".resultShow2").fadeIn(2000);
+      $(".replacedName").text(someNameInput);
     }
-    else if (sumOfChoices === "24") {
-      result = pcAndroid(compPreferences, phonePreferences);
+    else if (sumOfChoices === 0) {
+      $(".resultShow1").hide();
+      $(".resultShow2").hide();
+      $(".resultShow3").fadeIn(2000);
+      $(".replacedName").text(someNameInput);
     }
     event.preventDefault();
   });
