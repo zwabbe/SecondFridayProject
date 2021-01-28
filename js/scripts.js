dd4a92115abec024d49fb88a198a44f9714b6e5e
Jeremy Miller <gkstaff@gmail.com>
2021-01-28 15:13:55 -0800

    fixes syntax, seperates logic scripts, and README.

--- js/scripts.js
+++ js/scripts.js
@@ -10,29 +10,28 @@ $(document).ready(function () {
     const q4 = parseInt($("input:radio[name=q4]:checked").val());
     const q5 = parseInt($("input:radio[name=q5]:checked").val());
     const someNameInput = $("input#inputName").val();
-    let sumOfChoices = q1 + q2 + q3 + q4 + q5;
-
-    if (someNameInput === "") {
-      alert("Please enter your name!")
-    }
-    else if (sumOfChoices === 2 && 4) {
+    let sumOfChoices = (q1 + q2 + q3 + q4 + q5);
+    console.log(sumOfChoices); 
+    if (sumOfChoices === 2 || sumOfChoices === 4) {
       $(".resultShow2").hide();
       $(".resultShow3").hide();
       $(".resultShow1").fadeIn(2000);
       $(".replacedName").text(someNameInput);
     }
-    else if (sumOfChoices === 1 && 3) {
+    else if (sumOfChoices === 1 || sumOfChoices === 3) {
       $(".resultShow1").hide();
       $(".resultShow3").hide();
       $(".resultShow2").fadeIn(2000);
       $(".replacedName").text(someNameInput);
     }
     else if (sumOfChoices === 0) {
-      $(".resultShow1").hide();
-      $(".resultShow2").hide();
-      $(".resultShow3").fadeIn(2000);
-      $(".replacedName").text(someNameInput);
+    $(".resultShow1").hide();
+    $(".resultShow2").hide();
+    $(".resultShow3").fadeIn(2000);
+    $(".replacedName").text(someNameInput);
     }
-    event.preventDefault();
+    else !sumOfChoices;
+    alert("Please make sure to answer every option and try again!");
+    
   });
 });
